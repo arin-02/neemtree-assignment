971a3cbcc2feb7087439c93df130cea36776d18d
ARIN SINGH <90857324+arin-02@users.noreply.github.com>
2022-11-30 19:39:34 +0530

    push

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Firstscreen from './Components/Firstscreen'
 import Secondscreen from './Components/Secondscreen'
-
+import {Routes,Route} from 'react-router-dom'
+import Page from './Components/Page'
 function App() {
   return (
     <div className='main'>
-      <Firstscreen/>
-      {/* <Secondscreen/> */}
+      {/* <Page/> */}
+    <Routes>
+      <Route path="/" element={<Page/>}/>
+      <Route path="/secondpage" element={<Secondscreen/>}/>
+    </Routes>
     </div>
   )
 }
--- client/src/Components/Firstscreen.jsx
+++ client/src/Components/Firstscreen.jsx
@@ -4,30 +4,51 @@ import {useNavigate} from 'react-router-dom'
 
 
 function Firstscreen() {
-  
+  const navigate=useNavigate();
+
   const [file, setFile] = useState("");
-  const [display, setdisplay] = useState();
+
+
+  // const form = document.getElementById('form');
+
+  // form.addEventListener('submit', function(e) {
+  //  e.preventDefault();
+   
+   
+  // })
+
+
+  
+  const submitclick=()=>{
+    navigate('/secondpage');
+    // const payload = new FormData(form);
+  
+   fetch('http://localhost:5000/neemtree', {
+   method: 'POST',
+   body: payload,
+   })
+   .then(res => res.json())
+   .then(data => console.log(data))
+   
+  }
 
   const handleClick = (e) =>{
-    console.log(e.target.files[0].name)
-    
-    setFile(e.target.files[0].name);
+    // console.log(e.target.files[0].name)
+    setFile(e.target.files[0]);
 
     var x = document.getElementById("writtenpart");
-      var y= document.getElementById("btnn");
+      var y= document.getElementById("form");
     
       y.style.visibility="visible";
-      
       y.style.marginTop = "50px";
       
       if (x.style.display === "none") {
         x.style.display = "block";
       
-        
       } else {
         x.style.display = "none";
       
-  }
+    }
   // const handlebtn=()=>{
   // }
 
@@ -42,8 +63,8 @@ function Firstscreen() {
     <div className="top cursor-pointer text-4xl flex mt-5 justify-center">
     <input  className='text-sm opacity-0 w-8 h-10 cursor-pointer z-10 '  type="file" name="" id=""  accept='.xlsx , .xls' onChange={handleClick} />
     <div className="filename w-52 absolute mt-10 text-sm">
-    {/* {file} */}
-    {file}
+    
+    {file.name}
     </div>
     <div className="icon cursor-pointer z-100 absolute">
     <FaUpload />
@@ -55,7 +76,7 @@ function Firstscreen() {
     </div>
    <div id='btnparent' className="bt text-center">
 
-    <button  id='btnn'  className="bg-green-600 text-center invisible hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full">
+    <button id="form" onClick={submitclick}   className="bg-green-600 text-center invisible hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full"  >
     SUBMIT
   </button>
    </div>
